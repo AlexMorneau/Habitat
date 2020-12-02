aad7fb28d24646dc93075543aecd20e770a06bad
alex <alexmorneau1992@gmail.com>
2020-12-02 13:28:29 -0500

    fix 2

--- server.js
+++ server.js
@@ -19,13 +19,13 @@ app.use(expressLayouts);
 app.use(express.static('public'));
 
 // MONGODB INITIALIZATION
-const mongoose = require('mongoose');
-mongoose.connect(process.env.DATABASE_URL, {
-    useNewUrlParser: true
-});
-const db = mongoose.connection;
-db.on('error', error => console.error(error));
-db.once('open', () => console.log('connected to Mongoose'));
+// const mongoose = require('mongoose');
+// mongoose.connect(process.env.DATABASE_URL, {
+//     useNewUrlParser: true
+// });
+// const db = mongoose.connection;
+// db.on('error', error => console.error(error));
+// db.once('open', () => console.log('connected to Mongoose'));
 
 app.use('/', indexRouter);
 
